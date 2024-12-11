a69e49ba39fc7c77156f2415dc43c8575eced6ba
Andrey <idevtier@gmail.com>
2024-12-11 22:11:39 +0100

    add: Init tracing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use promptpunch::{llm::chat_gpt::ChatGptModel, prelude::*};
 use sqlx::AnyPool;
 use tokio::net::TcpListener;
+use tracing_subscriber::{filter::EnvFilter, util::SubscriberInitExt};
+
+use tracing_subscriber::layer::SubscriberExt as _;
 use wingb::{web, DatabaseOrbiter};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
     sqlx::any::install_default_drivers();
     let db_url = std::env::var("DATABASE_URL").expect("Failed to get DATABASE_URL");
 
