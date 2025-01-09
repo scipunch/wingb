47b5528a5be3b4d111b4306dc730de0961df0ae8
Andrey <idevtier@gmail.com>
2025-01-09 13:09:28 +0100

    add: DI

--- wingb.py
+++ wingb.py
@@ -1,49 +1,77 @@
+import dataclasses
+import inspect
 import json
 import logging
 import os
 import socket
 import sys
+import typing
 import urllib
 import urllib.parse
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from pathlib import Path
-from typing import Literal, NewType, Protocol
+from typing import (
+    Any,
+    Callable,
+    Literal,
+    NamedTuple,
+    NewType,
+    Protocol,
+    runtime_checkable,
+)
 
 import jinja2
-import psycopg2
+import psycopg
 
 logging.basicConfig(level=os.environ.get("LOG_LEVEL", logging.INFO))
 log = logging.getLogger(__name__)
 
-jinja_env = jinja2.Environment(loader=jinja2.PackageLoader("wingb"))
-
 #  begin-region -- Domain
 
 SqlValue = str | int | bool
+HTTPMethod = Literal["GET", "POST"]
 Htmx = NewType("Htmx", str)
 
 TEMPLATES_DIRECTORY = Path(os.getcwd()) / "templates/"
 assert os.path.exists(TEMPLATES_DIRECTORY)
 
 
+@runtime_checkable
 class SqlProvider(Protocol):
-    def fetch(self, query: str) -> tuple[list[str], list[SqlValue]]: ...
+    def execute(self, query: str) -> list[NamedTuple]: ...
 
 
 #  end-region   -- Domain
 
+#  begin-region -- Context factory
+
+
+def init_jinja2_env() -> jinja2.Environment:
+    return jinja2.Environment(loader=jinja2.PackageLoader("wingb"))
+
+
+def init_sql_provider() -> SqlProvider:
+    return PostgreSqlProvider(psycopg.connect(os.environ["DATABASE_URL"]))
+
+
+#  end-region   -- Context factory
+
 #  begin-region -- Web
 
 
-def get_root(req: BaseHTTPRequestHandler) -> Htmx:
+def get_root(
+    req: BaseHTTPRequestHandler,
+    sql_provider: SqlProvider,
+    jinja_env: jinja2.Environment,
+) -> Htmx:
     req.send_response(HTTPStatus.OK)
     req.send_header("Content-Type", "text/html")
     template = jinja_env.get_template("page/index.html")
     return Htmx(template.render())
 
 
-def post_generate(req: BaseHTTPRequestHandler) -> Htmx:
+def post_generate(req: BaseHTTPRequestHandler, jinja_env: jinja2.Environment) -> Htmx:
     length = req.headers.get("content-length")
     try:
         nbytes = int(length or "0")
@@ -74,7 +102,17 @@ class HTMXRequestHandler(BaseHTTPRequestHandler):
 
     protocol_version = "HTTP/1.1"
 
-    routes = {"GET": {"/": get_root}, "POST": {"/generate": post_generate}}
+    routes: dict[HTTPMethod, dict[str, Callable[..., Any]]] = {
+        "GET": {"/": get_root},
+        "POST": {"/generate": post_generate},
+    }
+    context_factory = [init_sql_provider, init_jinja2_env]
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.context: list[Any] = []
+        for factory in self.context_factory:
+            self.context.append(factory())
+        super().__init__(*args, **kwargs)
 
     def do_POST(self) -> None:
         content = self.process_request("POST")
@@ -93,14 +131,26 @@ class HTMXRequestHandler(BaseHTTPRequestHandler):
     def do_HEAD(self) -> None:
         self.process_request("GET")
 
-    def process_request(self, method: Literal["GET", "POST"]) -> bytes | None:
+    def process_request(self, method: HTTPMethod) -> bytes | None:
         route_handler = self.routes[method].get(self.path)
 
         if route_handler is None:
             self.send_response(HTTPStatus.NOT_FOUND)
             return None
 
-        content = route_handler(self)
+        typehints = typing.get_type_hints(route_handler)
+        del typehints["return"]
+        kwargs = {}
+        for arg_name, arg_class in typehints.items():
+            if arg_class == BaseHTTPRequestHandler:
+                kwargs[arg_name] = self
+                continue
+            log.info(f"{self.context=}, {arg_class=}")
+            kwargs[arg_name] = next(d for d in self.context if isinstance(d, arg_class))
+
+        log.info(f"{typehints=}")
+
+        content = route_handler(**kwargs)
         content_bytes = None
 
         if content:
@@ -112,6 +162,22 @@ class HTMXRequestHandler(BaseHTTPRequestHandler):
 
 
 #  end-region   -- Web
+
+#  begin-region -- Sql
+
+
+@dataclasses.dataclass
+class PostgreSqlProvider:
+    conn: psycopg.Connection[Any]
+
+    def execute(self, query: str) -> list[NamedTuple]:
+        with self.conn.cursor(row_factory=psycopg.rows.namedtuple_row) as cur:
+            rows = cur.execute(query).fetchall()
+
+        raise NotImplementedError
+
+
+#  end-region   -- Sql
 
 
 def main() -> None:
